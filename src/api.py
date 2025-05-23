3e2f0e85ae40a9585a16480149da0dfecf7de53a
Dakota <73153176+dakotatokad@users.noreply.github.com>
2025-05-23 15:04:28 -0600

    add basic functionality to inspire helldivers and issue the major order

---
+++ src/api.py
@@ -0,0 +1,37 @@
+import requests
+
+class HellDivers:
+    def __init__(self):
+        self.name = "HellDivers"
+        self.endpoint = "https://api.helldivers2.dev"
+        self.headers = {
+            "accept": "application/json",
+            "X-Super-Client": "hellbot",
+            "X-Super-Contact": "hellbot",
+        }
+        
+    def get_major_order(self) -> tuple[str, str, str, int]:
+        """
+        Gets the current Major Order.
+
+        Returns:
+            tuple[str, str, str, int]: The Major Order as
+                - briefing: The briefing for the Major Order
+                - rewards: The rewards for the Major Order
+                - expires: The expiration time for the Major Order
+                - code: The HTTP status code (200 for success)
+        """
+        response_assignments = requests.get(f"{self.endpoint}/api/v1/assignments", headers=self.headers)
+        code = response_assignments.status_code
+        if code == 200:
+            response = response_assignments.json()
+            briefing = response[0]["briefing"]
+            rewards = response[0]["reward"]["amount"]
+            expires = response[0]["expiration"]
+        else:
+            briefing = "Error: Unable to fetch Major Order"
+            rewards = "Error: Unable to fetch Major Order"
+            expires = "Error: Unable to fetch Major Order"
+            
+        return briefing, rewards, expires, code
+    
---
+++ src/utils.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timezone
+
+def days_from_now(date: str) -> str:
+    """
+    Calculates the number of days, hours, and minutes from now until the given date.
+
+    Args:
+        date (str): A date string in ISO 8601 format (e.g., "2025-05-27T10:18:17.8695449Z")
+
+    Returns:
+        str: The time from now until the given date, formatted as "X days, Y hours, Z minutes". No trailing punctuation.
+    """
+    from_now = str(
+            datetime.fromisoformat(date[:26]).replace(tzinfo=timezone.utc) 
+            - datetime.now(timezone.utc)
+        ).split(", ")
+    
+    days = from_now[0]
+    clock_time = from_now[1].strip().split(":")
+    hours = clock_time[0]
+    minutes = clock_time[1]
+    
+    return f"{days}, {hours} hours, and {minutes} minutes"
+    
+    
