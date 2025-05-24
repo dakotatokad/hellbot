1e286a4707f0e2bc4284eee70424366490e36b48
Dakota <73153176+dakotatokad@users.noreply.github.com>
2025-05-23 23:31:03 -0600

    implement cache getter and setter in public method

--- src/api.py
+++ src/api.py
@@ -20,11 +20,10 @@ class HellDivers:
             "X-Super-Client": super_client,
             "X-Super-Contact": super_contact,
         }
-        self._cache: dict = {} # { key: {data, timestamp}}
-        self._cache_ttl: int = 600 # seconds
-
+        self._cache: dict = {} # { key: {data, timestamp, ttl}}
         
-    def _get_from_cache(self, key: str) -> str | int | None:
+        
+    def _get_from_cache(self, key: str) -> dict[str, str] | None:
         """
         Based on a key, check if the data is in the cache. If it is, check if
         the data is still valid. If it is, return the data. If not, return None.
@@ -38,22 +37,23 @@ class HellDivers:
         # TODO: make sure dictionary is not empty
         entry = self._cache.get(key)
         if entry:
-            data, timestamp = entry
-            if time.time() - timestamp < self._cache_ttl:
+            data, timestamp, ttl = entry
+            if time.time() - timestamp < ttl:
                 return data
             
         return None
     
     
-    def _set_cache(self, key: str, data: str | int) -> None:
+    def _set_cache(self, key: str, data: dict[str, str], ttl: int = 600) -> None:
         """
         Set the cache data for a given key along with its timestamp.
 
         Args:
             key (str): The lookup key for the cached data.
             data (str | int): The data to be cached.
+            ttl (int): The time-to-live for the cache entry in seconds. Defaults to 600 seconds.
         """
-        self._cache[key] = (data, time.time())
+        self._cache[key] = (data, time.time(), ttl)
     
     
     def get_major_order(self) -> tuple[str, str, str, int]:
@@ -65,8 +65,18 @@ class HellDivers:
                 - briefing: The briefing for the Major Order
                 - rewards: The rewards for the Major Order
                 - expires: The expiration time for the Major Order
-                - code: The HTTP status code (200 for success)
+                - code: The HTTP status code (200 for success, 0 for cache)
         """
+        # Check if the Major Order is in the cache
+        cache_key = "major_order"
+        cached_info = self._get_from_cache(cache_key)
+        if cached_info:
+            # TODO: Add info log for cache hit
+            briefing = cached_info["briefing"]
+            rewards = cached_info["rewards"]
+            expires = cached_info["expires"]
+            return briefing, rewards, expires, 0
+        
         response_assignments = requests.get(f"{self.endpoint}/api/v1/assignments", headers=self.headers)
         code = response_assignments.status_code
         if code == 200:
@@ -79,5 +89,14 @@ class HellDivers:
             rewards = "Error: Unable to fetch Major Order"
             expires = "Error: Unable to fetch Major Order"
             
+        result = {
+            "briefing": briefing,
+            "rewards": rewards,
+            "expires": expires
+        }
+        
+        # Major orders are daily at minimum, so set ttl to 24 hours
+        self._set_cache(cache_key, result, 86400) 
+        
         return briefing, rewards, expires, code
     
